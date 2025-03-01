16d7642e6686db6480d4d0e40332654a74018f1e
sujith-1610 <sujithgundapu@gmail.com>
2025-03-01 22:31:10 +0530

    Update diffusion.py

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -269,7 +269,7 @@ class Diffusion(object):
 
         start_epoch, step = 0, 0
         if self.args.resume_training:
-            states = torch.load(os.path.join(self.args.log_path, "/kaggle/working/exp/logs/Fast-DDPM_experiments/ckpt_200.pth"))
+            states = torch.load(os.path.join(self.args.log_path, "/kaggle/working/exp/logs/Fast-DDPM_experiments/ckpt_300.pth"))
             print(f"✅ Loading checkpoint from latest_checkpoint")
             model.load_state_dict(states[0])
 
