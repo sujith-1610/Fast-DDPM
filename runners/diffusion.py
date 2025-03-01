be1fa0cb63c81da1c239ddea876d173fe900849c
sujith-1610 <sujithgundapu@gmail.com>
2025-03-01 22:18:08 +0530

    Update diffusion.py

--- runners/diffusion.py
+++ runners/diffusion.py
@@ -270,7 +270,7 @@ class Diffusion(object):
         start_epoch, step = 0, 0
         if self.args.resume_training:
             states = torch.load(os.path.join(self.args.log_path, "logs/pet_train_model.pth/ckpt_100.pth"))
-            print(f"✅ Loading checkpoint from {latest_checkpoint}")
+            print(f"✅ Loading checkpoint from latest_checkpoint")
             print(f"✅ Resumed training from Epoch {start_epoch}, Step {step}")
             model.load_state_dict(states[0])
 
